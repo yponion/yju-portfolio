2306f6154cb580d2d3636d8ff87e522de0382d8c
yponion <yp071704@gmail.com>
2025-01-22 22:48:01 +0900

    feat: projects 데이터 세팅

--- src/app/_components/projects/Projects.tsx
+++ src/app/_components/projects/Projects.tsx
@@ -1,6 +1,47 @@
 import Title from "../title/Title";
 import style from "./Projects.module.scss";
 
+interface Project {
+  title: string;
+  skills: string[];
+  date: string;
+  headcount: number;
+  introduce: string;
+}
+
+const data: Project[] = [
+  {
+    title: "yju-portfolio",
+    skills: ["Next.js", "TypeScript", "Sass"],
+    date: "2025.01-2025.02",
+    headcount: 1,
+    introduce:
+      "나만의 포트폴리오를 위한 웹사이트 제작. 모션 라이브러리 없이 직접 JS 또는 CSS로 구현",
+  },
+  {
+    title: "Qcraft",
+    skills: ["React", "TypeScript", "Redux"],
+    date: "2024.09-2024.12",
+    headcount: 2,
+    introduce: "AI 가상 인터뷰. Web Speech API를 활용해 음성답변 기능 구현",
+  },
+  {
+    title: "ProjectManagement BE",
+    skills: ["Node.js", "JavaScript", "MongoDB"],
+    date: "2024.04",
+    headcount: 1,
+    introduce:
+      "프로젝트 관리 웹사이트. 기존 팀프로젝트 ProjectManagement의 Backend 부분을 직접 구현",
+  },
+  {
+    title: "ProjectManagement FE",
+    skills: ["React", "JavaScript", "Redux"],
+    date: "2023.11-2024.12",
+    headcount: 2,
+    introduce: "프로젝트 관리 웹사이트.",
+  },
+];
+
 export default function Projects() {
   return (
     <div className={style.container}>
