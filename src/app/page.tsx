304eda90d4590345deba0958671a4f0b5e8b3be3
yponion <yp071704@gmail.com>
2025-01-23 14:07:37 +0900

    refactor: 사용하지 않는 클래스 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import style from "./page.module.scss";
 import Main from "./_components/main/Main";
 import AboutMe from "./_components/aboutme/AboutMe";
 import Skills from "./_components/skills/Skills";
@@ -7,12 +6,12 @@ import Projects from "./_components/projects/Projects";
 
 export default function Page() {
   return (
-    <div className={style.container}>
+    <>
       <Main />
       <AboutMe />
       <Skills />
       <Archiving />
       <Projects />
-    </div>
+    </>
   );
 }
