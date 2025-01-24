32df96d32e9c3b0b7cf37ed13f6469fdc63fa4cc
yponion <yp071704@gmail.com>
2025-01-24 15:01:17 +0900

    feat: 최상단으로 가는 버튼 추가

--- src/app/_components/nav/Nav.tsx
+++ src/app/_components/nav/Nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import style from "./Nav.module.scss";
 import { useScrollPosition } from "@/app/_hooks/useScrollPosition";
 import useIsMobile from "@/app/_hooks/useIsMobile";
@@ -11,42 +11,81 @@ export default function Nav() {
   const { currentY, previousY } = useScrollPosition();
   const isMobile = useIsMobile();
   const [showMenu, setShowMenu] = useState(false);
+  const timer = useRef<NodeJS.Timeout | null>(null);
+  const [showUp, setShowUp] = useState(false);
+  const [showUpHover, setShowUpHover] = useState(false);
 
   const onClickMenu = () => {
     setShowMenu(!showMenu);
   };
 
+  useEffect(() => {
+    if (currentY < 100) {
+      if (showUp) setShowUp(false);
+      if (timer.current) clearTimeout(timer.current);
+      return;
+    }
+    if (showUpHover) {
+      if (timer.current) clearTimeout(timer.current);
+      return;
+    }
+    if (timer.current) clearTimeout(timer.current);
+    setShowUp(true);
+    timer.current = setTimeout(() => {
+      setShowUp(false);
+      timer.current = null;
+    }, 2000);
+  }, [currentY, showUpHover]);
+
   return (
-    <div
-      className={`${style.container}
+    <>
+      <div
+        className={`${style.container}
        ${currentY < previousY || currentY < 72 ? style.show : style.hide} ${
-        currentY > 172 ? style.navBG : null
-      }`}
-    >
-      <div className={style.navWraper}>
-        <div className={style.logo}>YJU&apos;s Portfolio</div>
-        <>
-          <ul className={`${style.ul} ${showMenu ? style.show : style.hide}`}>
-            {menu.map((v, i) => (
-              <li key={i} className={style.li}>
-                <Link href={`#${v}`}>{v}</Link>
-              </li>
-            ))}
-          </ul>
-          {isMobile ? (
-            <div className={style.menuBtn} onClick={onClickMenu}>
-              <div className={style.lineBox}>
-                {["top", "mid", "bot"].map((v, i) => (
-                  <div
-                    key={i}
-                    className={`${style[v]} ${showMenu ? style.open : ""}`}
-                  ></div>
-                ))}
+          currentY > 172 ? style.navBG : null
+        }`}
+      >
+        <div className={style.navWraper}>
+          <div className={style.logo}>YJU&apos;s Portfolio</div>
+          <>
+            <ul className={`${style.ul} ${showMenu ? style.show : style.hide}`}>
+              {menu.map((v, i) => (
+                <li key={i} className={style.li}>
+                  <Link href={`#${v}`}>{v}</Link>
+                </li>
+              ))}
+            </ul>
+            {isMobile ? (
+              <div className={style.menuBtn} onClick={onClickMenu}>
+                <div className={style.lineBox}>
+                  {["top", "mid", "bot"].map((v, i) => (
+                    <div
+                      key={i}
+                      className={`${style[v]} ${showMenu ? style.open : ""}`}
+                    ></div>
+                  ))}
+                </div>
               </div>
-            </div>
-          ) : null}
-        </>
+            ) : null}
+          </>
+        </div>
       </div>
-    </div>
+      <button
+        style={showUp ? { display: "block" } : { display: "none" }}
+        className={style.up}
+        onClick={() => window.scrollTo(0, 0)}
+        onMouseEnter={() => setShowUpHover(true)}
+        onMouseLeave={() => setShowUpHover(false)}
+      >
+        <svg
+          height={30}
+          width={30}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+        >
+          <path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z" />
+        </svg>
+      </button>
+    </>
   );
 }
