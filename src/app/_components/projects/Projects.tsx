982d71bd7fb0d77a5a5d4f424b029c54e5c6948d
yponion <yp071704@gmail.com>
2025-01-25 02:46:41 +0900

    feat: project 데이터 추가

--- src/app/_components/projects/Projects.tsx
+++ src/app/_components/projects/Projects.tsx
@@ -9,10 +9,15 @@ const data: Project[] = [
     skills: ["Next.js", "TypeScript", "Sass"],
     src: process.env.NEXT_PUBLIC_BASE_URL as string,
     images: [],
-    date: "2025.01 - 2025.02",
+    date: "2025.01",
     headcount: 1,
-    introduce:
-      "나만의 포트폴리오를 위한 웹사이트 제작. 모션 라이브러리 없이 직접 JS 또는 CSS로 구현",
+    introduce: `나만의 포트폴리오를 위한 웹사이트 제작.
+
+모션 라이브러리 없이 직접 JS 또는 CSS로 구현.
+Sass를 이용하여 스타일링.
+Zustand를 이용한 상태관리.
+react-qr-code을 이용하여 연락처 QR코드로 생성.
+swiper를 이용하여 이미지 무한크롤을 간단하게 구현.`,
     github: "https://github.com/yponion/yju-portfolio",
   },
   {
@@ -28,7 +33,12 @@ const data: Project[] = [
     ],
     date: "2024.09 - 2024.12",
     headcount: 2,
-    introduce: "AI 가상 인터뷰. Web Speech API를 활용해 음성답변 기능 구현",
+    introduce: `AI 가상 인터뷰.
+    
+자기소개서, 입사지원서 따위의 pdf, docs, hwp 파일을 입력하면 AI가 분석하여 면접 예상 질문을 뽑아주고 답변에대한 피드백 까지 해주는 웹 애플리케이션.
+Web Speech API를 활용해 음성답변 기능 구현.
+
+현재 Claude API 등 과금 문제로 Backend server는 내려가있음.`,
     github: "https://github.com/Capstone-QCraft/client",
   },
   {
@@ -39,7 +49,7 @@ const data: Project[] = [
     date: "2024.04",
     headcount: 1,
     introduce:
-      "프로젝트 관리 웹사이트. 기존 팀프로젝트 ProjectManagement의 Backend 부분을 직접 구현",
+      "프로젝트 관리 웹사이트.\n\n기존 팀프로젝트 ProjectManagement의 Backend 부분을 Node.js와 MongoDB를 이용하여 직접 구현하며 Backend에 대한 이해도 상승.",
     github: "https://github.com/yponion/ProjectManagement",
   },
   {
@@ -49,7 +59,7 @@ const data: Project[] = [
     images: [],
     date: "2023.11 - 2024.12",
     headcount: 2,
-    introduce: "프로젝트 관리 웹사이트.",
+    introduce: "프로젝트 관리 웹사이트.\n\n본격적인 웹 개발 입문.",
     github: "https://github.com/yponion/ProjectManagement",
   },
 ];
