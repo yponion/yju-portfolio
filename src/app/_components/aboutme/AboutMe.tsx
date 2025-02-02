ebf5d7d991b9b5ad873be74a42174b1e3154b0a0
yponion <yp071704@gmail.com>
2025-02-02 21:14:31 +0900

    fix: Date가 배포 시점으로 고정되어서 요청 시점에 데이터를 가져오고 페이지의 내용을 렌더링하는 데 사용할 수 있는 getServerSideProps 사용

--- src/app/_components/aboutme/AboutMe.tsx
+++ src/app/_components/aboutme/AboutMe.tsx
@@ -2,38 +2,44 @@ import Timeline from "./components/timeline/Timeline";
 import Title from "../title/Title";
 import style from "./AboutMe.module.scss";
 
-const now = new Date();
-const year = now.getFullYear();
-const month = String(now.getMonth() + 1).padStart(2, "0");
-const formattedDate = `${year}.${month}`;
-
 interface Data {
   title: string;
   date: string;
   content?: string;
 }
 
-const data: Data[] = [
-  { title: "출생", date: "2000.03" },
-  { title: "영천중학교 졸업", date: "2017.02" },
-  { title: "영천고등학교 졸업", date: "2019.02" },
-  { title: "정보처리기사 취득", date: "2024.09" },
-  {
-    title: "국립공주대학교 졸업",
-    date: "2025.02",
-    content: "컴퓨터공학부 소프트웨어전공",
-  },
-  {
-    title: "입사 지원",
-    date: formattedDate,
-    content:
-      "빠르게 변화하는 기술 환경 속에서도 끊임없이 학습하며, 팀과의 적극적인 협업을 통해 문제를 효율적으로 해결하는 개발자가 되겠습니다.",
-  },
-];
+export async function getServerSideProps() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const formattedDate = `${year}.${month}`;
 
-data.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+  const data: Data[] = [
+    { title: "출생", date: "2000.03" },
+    { title: "영천중학교 졸업", date: "2017.02" },
+    { title: "영천고등학교 졸업", date: "2019.02" },
+    { title: "정보처리기사 취득", date: "2024.09" },
+    {
+      title: "국립공주대학교 졸업",
+      date: "2025.02",
+      content: "컴퓨터공학부 소프트웨어전공",
+    },
+    {
+      title: "입사 지원",
+      date: formattedDate,
+      content:
+        "빠르게 변화하는 기술 환경 속에서도 끊임없이 학습하며, 팀과의 적극적인 협업을 통해 문제를 효율적으로 해결하는 개발자가 되겠습니다.",
+    },
+  ];
 
-export default function AboutMe() {
+  data.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+
+  return {
+    props: { data },
+  };
+}
+
+export default function AboutMe({ data }: { data: Data[] }) {
   return (
     <section id="AboutMe" className={style.container}>
       <Title title="AboutMe" />
