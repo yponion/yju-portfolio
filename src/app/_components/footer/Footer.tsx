06db2e0cb847e162dbab5fe0f407ddf169c8d3df
양정운 <yp071704@gmail.com>
2026-01-02 15:05:50 +0900

    feat: 저작권 년도 변경

--- src/app/_components/footer/Footer.tsx
+++ src/app/_components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import style from "./Footer.module.scss";
 export default function Footer() {
   return (
     <div className={style.container}>
-      <span>© 2025. Yang jeong un. All rights reserved.</span>
+      <span>© 2026. Yang jeong un. All rights reserved.</span>
     </div>
   );
 }
