b79743f930c92424e869abe9351a4e6a99220448
양정운 <yp071704@gmail.com>
2026-01-13 00:04:47 +0900

    feat: iframe 에 title 을 부여하여 웹 접근성 개선

--- src/app/_components/projects/components/card/Card.tsx
+++ src/app/_components/projects/components/card/Card.tsx
@@ -25,7 +25,7 @@ export default function Card({ project }: Props) {
     <div ref={ref} className={`${style.card} ${active ? style.active : ""}`} onClick={() => openModal(project)}>
       <div className={style.viewBox}>
         {project.title === "yju-portfolio" ? (
-          <iframe src={project.src} />
+          <iframe src={project.src} title="portfolioFrame" />
         ) : (
           <Image className={style.img} fill src={project.src} alt={project.title + "이미지"} />
         )}
